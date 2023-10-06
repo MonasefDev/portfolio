0676d34ef72e0d2049b6df62690ca673a2768a39
Monasef <monasef77@gmail.com>
2023-10-06 19:12:01 +0100

    add server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, "build")));
+
+// Handle requests for client-side routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
